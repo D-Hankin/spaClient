817c6a58ae746e15662218471f7ed41168e2b783
David Hankin <d_hankin@outlook.com>
2024-04-08 15:03:20 +0200

    home template

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <h2>Welcome</h2>
+      <h3>A hard day torturing the souls of the damned? We offer two relaxation treatments sure to sooth any aching scales!</h3>
+      <div>Option 1</div>
+      <div>Option 2</div>
+      <p>Make a booking using the menu on the left!</p>
+    </>
   )
 }
 
