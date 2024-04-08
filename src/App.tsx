5e04dd5cc8a70d5ec760389028ebe5379b4f6e2f
David Hankin <d_hankin@outlook.com>
2024-04-08 14:10:59 +0200

    First commit, vite framework

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>The New World Order Spa and Relaxtion Center</h1>
+
+    </>
+  )
+}
+
+export default App
