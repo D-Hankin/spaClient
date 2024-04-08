0f985bd1fd4e8cd242c5846e434504c2f98bef67
David Hankin <d_hankin@outlook.com>
2024-04-08 14:29:25 +0200

    initital components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import './App.css'
+import Menu from './menu/Menu'
+import Staff from './staff/Staff'
+import Booking from './booking/Booking'
+import Contact from './contact/Contact'
 
 function App() {
 
   return (
     <>
       <h1>The New World Order Spa and Relaxtion Center</h1>
-
+      <div id='contentDiv'>
+        <div id='menuLeftDiv'>
+          <Menu />
+        </div>
+        <div id='contentRightDiv'>
+          <Staff />
+          <Booking />
+          <Contact />
+        </div>
+      </div>
     </>
   )
 }
