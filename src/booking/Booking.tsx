014df809dab86a15c2a2ee0b659deefad0e78b9d
David Hankin <d_hankin@outlook.com>
2024-04-09 09:07:05 +0200

    React calender added, renaming of caledner component

--- src/booking/Booking.tsx
+++ src/booking/Booking.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import CalenderComponent from './calenderComponent/calenderComponent'
 
 function Booking() {
   return (
     <>
       <h2>Welcome to the booking page!</h2>
       <h3>Choose a date and treatment to see available sessions</h3>
+      <CalenderComponent key="calender"/>
     </>
   )
 }
---
+++ src/booking/calenderComponent/calenderComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Calendar from 'react-calendar'
+
+function calender() {
+  return (
+    <Calendar />
+
+  )
+}
+
+export default calender
