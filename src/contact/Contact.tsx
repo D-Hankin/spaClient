755ed375702d353546e9dea5219ad73eb7336619
David Hankin <d_hankin@outlook.com>
2024-04-23 15:38:30 +0200

    try image as import

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -1,4 +1,5 @@
 import "./contact.css"
+import devil from "./images/Devil.jpg";
 
 function Contact() {
   return (
@@ -6,7 +7,7 @@ function Contact() {
       <h1>Contact</h1>
       <h3>The Lord Master is always watching! If he wishes to speak to you, he will speak to you.</h3>
       <div id="devilDiv">
-        <img id="imgDiv" src='src\contact\images\Devil.jpg'></img>
+        <img id="imgDiv" src={ devil }></img>
       </div>
     </div>
   )
